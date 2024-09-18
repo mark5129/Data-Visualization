688a6ab9ae603fc161049f78a567f3a04274dd7e
mark5129 <markus.kaad.heuer@gmail.com>
2024-09-19 00:34:47 +1000

    Map finished

--- Australia_map.js
+++ Australia_map.js
@@ -2,12 +2,12 @@ const map_projection = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json", 
     "width": 800,
     "height": 450,
-    "title": "Number of flights going to Australian cities",
+    "title": "Number of flights going from Australian cities",
     "projection": {"type": "equalEarth"}, 
     "layer": [
         {
         "data": {
-            "url": "https://raw.githubusercontent.com/mark5129/Data-Visualization/main/ne_110m_admin_0_countries.topojson",
+            "url": "https://raw.githubusercontent.com/mark5129/Data-Visualization/main/Australiamap.topojson",
             "format": {"type": "topojson", "feature": "ne_110m_admin_0_countries"}
         },
         "mark": {
@@ -36,9 +36,20 @@ const map_projection = {
                     "type": "quantitative"
                 },
                 "size": {
-                "field": "All_Flights",
-                "type": "quantitative",
-                } 
+                    "field": "All_Flights",
+                    "type": "quantitative",
+                    "scale" : {
+                        "type" : "linear",
+                        "domain" : [ 0, 20000, 40000, 60000, 80000 ],
+                        "range" : [ 100 , 400 , 700 , 1000 , 1300]},
+                },
+                "tooltip": [
+                    {"field": "Australian_City", "type": "nominal", "title": "Australian City"},
+                    {"field": "AUS_State", "type": "nominal", "title": "Australian State"},
+                    {"field": "AUS_Population", "type": "quantitative", "title": "Population", "format": ","},
+                    {"field": "All_Flights", "type": "quantitative", "title": "Total Flights", "format": ","},
+                    {"field": "Total_Seats", "type": "quantitative", "title": "Total Seats", "format": ","}
+                ]
             }
         } 
     ]
